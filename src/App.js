da26263da3883079a04d556b7e44c24097ac2d89
cunhavcs <cunha.vcs@gmail.com>
2022-12-07 19:49:15 -0300

    03 - Cria e renderiza o componente Card com as props necessárias

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
-    return (<Form />);
+    return (
+      <>
+        <Form />
+        <Card />
+      </>
+    );
   }
 }
 
