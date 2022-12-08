1408ad9aa310ff39a06e40d5f7c692d42a82bdb2
cunhavcs <cunha.vcs@gmail.com>
2022-12-08 15:07:27 -0300

    04 - Cria o preview da carta que está sendo criada pelo formulário

--- src/App.js
+++ src/App.js
@@ -3,11 +3,40 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  state = {
+    'name-input': '',
+    'description-input': '',
+    'attr1-input': '',
+    'attr2-input': '',
+    'attr3-input': '',
+    'image-input': '',
+    'rare-input': 'normal',
+    'trunfo-input': false,
+  };
+
+  onInputChange = ({ target }) => {
+    const { id } = target;
+    const value = (target.type === 'checkbox') ? target.checked : target.value;
+    this.setState({
+      [id]: value,
+    });
+  };
+
   render() {
+    const { state } = this;
     return (
       <>
-        <Form />
-        <Card />
+        <Form onInputChange={ this.onInputChange } />
+        <Card
+          cardName={ state['name-input'] }
+          cardImage={ state['image-input'] }
+          cardDescription={ state['description-input'] }
+          cardAttr1={ state['attr1-input'] }
+          cardAttr2={ state['attr2-input'] }
+          cardAttr3={ state['attr3-input'] }
+          cardRare={ state['rare-input'] }
+          cardTrunfo={ state['trunfo-input'] }
+        />
       </>
     );
   }
